34e81650fea0ce889831df3a184bb1b99aff38a6
Simon <alers@evospark.de>
2023-01-24 02:53:15 +0100

    Added connection Line Visual

---
+++ Assets/TangibleLine.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TangibleLine : MonoBehaviour
+{
+    public Tangible tangible0;
+    public Tangible tangible1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
